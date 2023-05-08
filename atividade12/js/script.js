95867f832792c30738a3a8c538842e8d44c3ff28
Antonio Carlos Fernandes Gomes Filho <antoniocarlosfilho2003@gmail.com>
2023-05-08 11:08:14 -0300

    08/05 - Eventos JS

---
+++ atividade12/js/script.js
@@ -0,0 +1,14 @@
+const janela = document.getElementById("janela");
+const texto = document.getElementById("lblJanela");
+
+function janelaAberta(){
+    janela.src = "../img/janelaaberta.png";
+}
+
+function janelaQuebrada(){
+    janela.src = "../img/janelaquebra.png";
+}
+
+function janelaFechada(){
+    janela.src = "../img/janelafechada.png";
+}
